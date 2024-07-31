adf9b7b43cf59cdde54199db4bde44e26f51197b
Tomasz Goluch <goluch@eti.pg.edu.pl>
2024-07-31 16:31:02 +0200

    added migration and created db

--- PolesDB/Data/AppDbContext.cs
+++ PolesDB/Data/AppDbContext.cs
@@ -5,6 +5,10 @@ namespace DataBase.Data
 {
     public class AppDbContext : DbContext
     {
+        //EF tools commands (set WebApp as startup project):
+        //Add-Migration InitialCreate
+        //Update-Database
+        //Drop-Database
         public DbSet<Person> Persons { get; set; }
         public DbSet<Company> Companies { get; set; }
         public DbSet<Employment> Contracts { get; set; }
