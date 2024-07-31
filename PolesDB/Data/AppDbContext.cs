12e94df8109a1b815b263ed4618904f0cf1047ab
Tomasz Goluch <goluch@eti.pg.edu.pl>
2024-07-31 16:06:29 +0200

    added DB context

---
+++ PolesDB/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using DataBase.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataBase.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Employment> Contracts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("DataSource=./PolesDB.db");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
