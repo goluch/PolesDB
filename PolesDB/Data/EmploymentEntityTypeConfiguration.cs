58db549eaaf19625166e1d8c1f70bcae0b70e6e1
Tomasz Goluch <goluch@eti.pg.edu.pl>
2024-08-02 15:07:08 +0200

    added configuration for employment entity

---
+++ PolesDB/Data/EmploymentEntityTypeConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using DataBase.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace PolesDB.Data
+{
+    internal class EmploymentEntityTypeConfiguration : IEntityTypeConfiguration<Employment>
+    {
+        public void Configure(EntityTypeBuilder<Employment> builder)
+        {
+            builder.OwnsOne(e => e.Contract);
+        }
+    }
+}
